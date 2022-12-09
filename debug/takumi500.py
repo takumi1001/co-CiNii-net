bed1e8605fccfe1931e464e1d8ef5deec315858e
Takumi Tamura <tamutamuphone@gmail.com>
2022-12-09 22:08:39 +0900

    del

--- debug/takumi500.py
+++
@@ -1,6 +0,0 @@
-from core.cociniinet import CoCiNiiNet
-
-if __name__ == "__main__":
-    net = CoCiNiiNet("https://cir.nii.ac.jp/crid/1070012545625749888", "田村匠", 0.5)
-    net.generate(max_reqests=500)
-    net.write_graphml("takumi500.graphml")
